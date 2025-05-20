2ee9598b4bb2bc3a7784d31506fe9461f6c7d9aa
ahnaf-npl <anhaf@npl.asia>
2025-05-20 16:04:43 +0800

    check video URLs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,22 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_ZAPIER_WEBHOOK_URL: process.env.ZAPIER_WEBHOOK_URL,
   },
+  // Tambahkan blok ini untuk meningkatkan batas ukuran payload API
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb", // Anda bisa mencoba '10mb', '50mb', atau '100mb'
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig; // Pastikan ini adalah `module.exports`
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     NEXT_PUBLIC_ZAPIER_WEBHOOK_URL: process.env.ZAPIER_WEBHOOK_URL,
+//   },
+// };
+
+// export default nextConfig;
